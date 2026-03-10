ca3d10b399a8128b620ec16130c142f2b8d1865c
Yago Christ <goga85521@gmail.com>
2026-03-09 22:08:36 -0300

    Update Firebase configuration with real credentials

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,13 @@ import { initializeApp } from 'firebase/app'
 import { getFirestore, collection, addDoc, getDocs, updateDoc, deleteDoc, doc, query, orderBy, onSnapshot } from 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDummyKeyForDemo",
+  apiKey: "AIzaSyAlEvh2t4F3Ys34wweLT2G57_CMBi_7R7M",
   authDomain: "convite-18-anos.firebaseapp.com",
   projectId: "convite-18-anos",
-  storageBucket: "convite-18-anos.appspot.com",
-  messagingSenderId: "123456789",
-  appId: "1:123456789:web:abcdef123456"
+  storageBucket: "convite-18-anos.firebasestorage.app",
+  messagingSenderId: "733524388932",
+  appId: "1:733524388932:web:2af831ad7d92d9d4a3a011",
+  measurementId: "G-C6XR7VH007"
 }
 
 const app = initializeApp(firebaseConfig)
